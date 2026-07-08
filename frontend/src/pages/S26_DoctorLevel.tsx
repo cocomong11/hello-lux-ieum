1dbfc3cf7ae5a6e5079b5f5ca82d0d984a1ee019
minseoj03 <minsejeon24@sookmyung.ac.kr>
2026-07-08 16:59:59 +0900

    feat: add doctorHeader and implement S23, S24, S26

---
+++ frontend/src/pages/S26_DoctorLevel.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/doctorHeader';
+
+export default function S26_DoctorLevel() {
+  const navigate = useNavigate();
+  return (
+    <div style={{
+              width: '100%', minHeight: '100vh', backgroundColor: '#FFFFFF',
+              fontFamily: 'Pretendard Variable, Inter, sans-serif',
+              display: 'flex', flexDirection: 'column', alignItems: 'center',
+              boxSizing: 'border-box', paddingBottom: '120px', position: 'relative'
+            }}>
+                <Header/>
+      <div className='min-h-screen flex items-center justify-center text-neutral-10 text-[28px] font-bold'>
+        S26_DoctorLevel
+      </div>
+    </div>
+  );
+}
